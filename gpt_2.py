d50ed324954bc8362ba3f9236ac007f45508087b
Bitan Biswas <76226078+bitanb1999@users.noreply.github.com>
2022-05-08 11:52:33 +0530

    Update gpt_2.py

--- gpt_2.py
+++ gpt_2.py
@@ -7,7 +7,18 @@ Original file is located at
     https://colab.research.google.com/drive/1b359TZHn6DmG2Ap8qeKvzssjfRgwEykk
 """
 
-import transformers
+def install_and_import(package):
+    import importlib
+    try:
+        importlib.import_module(package)
+    except ImportError:
+        import pip
+        pip.main(['install', package])
+    finally:
+        globals()[package] = importlib.import_module(package)
+
+
+install_and_import('transformers')
 
 #for reproducability
 SEED = 34
